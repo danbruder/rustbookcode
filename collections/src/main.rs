1bcc4a53123f2830202f9109d72784c2b7c16891
Dan Bruder <dbruder@rapiddg.com>
2018-04-19 23:03:23 -0400

    collections

---
+++ collections/src/main.rs
@@ -0,0 +1,20 @@
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
+fn main() {
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    for i in row {
+        match i {
+            SpreadsheetCell::Int(v) => println!("Int: {}", v + 1),
+            SpreadsheetCell::Text(v) => println!("Text: {}", v),
+        }
+    }
+}
