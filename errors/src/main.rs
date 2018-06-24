5f60568aaba96b55cb44ae2faf65fd9410429962
Dan Bruder <dbruder@rapiddg.com>
2018-06-23 23:37:52 -0400

    Errors

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,3 +1,48 @@
 fn main() {
-    println!("Hello, world!");
+    //panic!("crash and burn!");
+    //let v = vec![1, 2, 3];
+
+    //v[99];
+    //use std::fs::File;
+
+    //let file = File::open("hello.txt");
+
+    //let f = match file {
+    //Ok(file) => file,
+    //Err(error) => panic!("There was a problem opening the file: {:?}", error),
+    //};
+
+    //use std::fs::File;
+    //use std::io::ErrorKind;
+
+    //let f = File::open("hello.txt");
+
+    //let f = match f {
+    //Ok(file) => file,
+    //Err(ref error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") {
+    //Ok(fc) => fc,
+    //Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+    //},
+    //Err(error) => panic!("There was a problem opening the file: {:?}", error),
+    //};
+    //use std::fs::File;
+    //let f = File::open("hello.txt")?;
+
+    // Guess
+    pub struct Guess {
+        value: u32,
+    }
+
+    impl Guess {
+        pub fn new(value: u32) -> Guess {
+            if value < 1 || value > 100 {
+                panic!("Gues must be in range");
+            }
+
+            Guess { value }
+        }
+        pub fn value(&self) -> u32 {
+            self.value
+        }
+    }
 }
