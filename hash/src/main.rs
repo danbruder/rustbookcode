2fe543ce73b3a11ad58b6f1c0b0df9cff38e7818
Dan Bruder <dbruder@rapiddg.com>
2018-06-23 23:03:40 -0400

    progress

---
+++ hash/src/main.rs
@@ -0,0 +1,13 @@
+#![allow(unused_variables)]
+fn main() {
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    for (key, value) in &scores {
+        println!("{} {}", key, value);
+    }
+}
